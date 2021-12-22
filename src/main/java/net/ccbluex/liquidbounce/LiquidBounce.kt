280e67e0bba58ae6f7a7b0c7f581fa35304dce3d
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2021-12-22 17:03:37 +0000

    Update LiquidBounce.kt

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -33,7 +33,7 @@ object LiquidBounce {
 
     // Client information
     const val CLIENT_NAME = "LiquidBounce"
-    const val CLIENT_VERSION = "Christmas Edition"
+    const val CLIENT_VERSION = "b74"
     const val CLIENT_CREATOR = "CheaterMC"
     const val CLIENT_CLOUD = "https://wysi-foundation.github.io/LiquidCloud/LiquidBounce"
 
