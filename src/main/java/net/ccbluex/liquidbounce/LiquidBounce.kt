6bd18b6a04edf86af715f9bbb275ee3bf6c11cb1
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2021-12-23 00:50:44 +0000

    crashed :(

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -33,7 +33,7 @@ object LiquidBounce {
 
     // Client information
     const val CLIENT_NAME = "LiquidBounce"
-    const val CLIENT_VERSION = "b74"
+    const val CLIENT_VERSION = "23122021"
     const val CLIENT_CREATOR = "CheaterMC"
     const val CLIENT_CLOUD = "https://wysi-foundation.github.io/LiquidCloud/LiquidBounce"
 
